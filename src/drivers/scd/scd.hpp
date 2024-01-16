265a530ed61eb8328c914741e3de0793f6decab5
Sun Yi <15531855673@163.com>
2024-01-16 09:27:19 +0800

    添加超声波用于可重构避障

---
+++ src/drivers/scd/scd.hpp
@@ -0,0 +1,58 @@
+/**
+ * 针对SCD_40的I2C驱动
+ */
+
+#pragma once
+
+#include <float.h>
+
+#include <drivers/drv_hrt.h>
+#include <lib/drivers/device/i2c.h>
+#include <lib/perf/perf_counter.h>
+#include <px4_platform_common/i2c_spi_buses.h>
+#include <uORB/PublicationMulti.hpp>
+#include <uORB/topics/scd.h>
+
+static constexpr uint32_t I2C_SPEED = 100 * 1000; // 传输速度
+static constexpr uint8_t I2C_ADDRESS_DEFAULT = 0x57; //设备地址0x31
+
+#define START_CMD_DOWM	0x01	//开始命令，低8位
+//#define START_CMD_UP	uint8_t(0x21)	//开始命令，高8位
+//#define READ_CMD_DOWN	uint8_t(0x05)	//读取命令，低8位
+//#define READ_CMD_UP	uint8_t(0xec)	//读取命令，高8位
+
+
+#define MIN_ACCURATE_DIFF_PRES_PA 0
+
+/* Measurement rate is 100Hz */
+#define CONVERSION_INTERVAL	(1000000 / 100)	/* microseconds */
+
+class Scd : public device::I2C, public I2CSPIDriver<Scd>
+{
+public:
+	Scd(const I2CSPIDriverConfig &config);
+	~Scd() override;
+
+	static void print_usage();
+
+	void RunImpl();
+
+	int init() override;
+
+private:
+	int probe() override;
+
+	int measure();
+	int collect();
+
+	uint32_t _measure_interval{CONVERSION_INTERVAL};
+	uint32_t _conversion_interval{CONVERSION_INTERVAL};
+
+	bool _sensor_ok{false};
+	bool _collect_phase{false};
+
+	uORB::PublicationMulti<scd_s> scd_pub{ORB_ID(scd)};
+
+	perf_counter_t _sample_perf{perf_alloc(PC_ELAPSED, MODULE_NAME": read")};
+	perf_counter_t _comms_errors{perf_alloc(PC_COUNT, MODULE_NAME": communication errors")};
+};
---
+++ src/modules/first/first.h
@@ -0,0 +1,59 @@
+/**
+ * @file adrc.h
+ *
+ * Definition of generic ADRC controller.
+ *
+ * @author Sunyi
+ */
+
+#ifndef FIRST_H_
+#define FIRST_H_
+#include <stdint.h>
+#include<px4_platform_common/module.h>
+#include <px4_platform_common/module_params.h>
+#include<uORB/topics/actuator_controls.h>
+#include <uORB/topics/pwm_input.h>
+#include <uORB/Subscription.hpp>
+#include <uORB/Publication.hpp>
+#include <uORB/topics/parameter_update.h>
+#include <uORB/topics/scd.h>
+
+
+__BEGIN_DECLS
+
+class First : public ModuleBase<First>, public ModuleParams
+{
+public:
+	First();
+	~First() override;
+	static int task_spawn(int argc, char*argv[]);
+	void run()override;
+	static First *instantiate(int argc, char *argv[]);
+	static int custom_command(int argc, char *argv[]);
+	static int print_usage(const char *reason = nullptr);
+
+
+	struct actuator_controls_s	_actuators2;
+	uORB::Subscription sr_sub{ORB_ID(scd)};
+	struct scd_s sr_value;
+
+	uORB::Subscription _params_sub{ORB_ID(parameter_update)};			/**< parameter updates subscription */
+	uORB::Publication<actuator_controls_s> _act2_pub{ORB_ID(actuator_controls_2)};
+
+private:
+	float angle_1;
+	float angle_2;
+	float angle_3;
+	float angle_4;
+	float dis_tance;
+
+		DEFINE_PARAMETERS(
+		(ParamInt<px4::params::ANGLE_ONE>) _param_angle_one,/*The angle of four arms*/
+		(ParamInt<px4::params::ANGLE_TWO>) _param_angle_two,
+		(ParamInt<px4::params::ANGLE_THREE>) _param_angle_three,
+		(ParamInt<px4::params::ANGLE_FOUR>) _param_angle_four,
+		(ParamInt<px4::params::DISTANCE>) _param_distance
+	)
+};
+__END_DECLS
+#endif
